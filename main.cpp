6569c292b114654e0706e9fd35010f8dce06b0c9
Eugen Hildt <eugen.hildt@posteo.de>
2021-07-06 21:29:57 +0100

    wip

--- main.cpp
+++ main.cpp
@@ -10,18 +10,21 @@ using std::cout;
 using std::string;
 using std::to_string;
 
+#define I2C_PWM_FAN_CHIP_ADDRESS 0x2f
+#define I2C_PWM_FAN_CHIP_REGISTER 0x30
+#define I2C_PWM_FAN_INIT_VALUE 0x66
+#define FAN_I2C_CHL "/dev/i2c-10"
+#define FAN_MAX_RPM 5000
+#define FAN_MAX_DBA 19.6
+
 int main(void) {
   struct timespec SLEEP_TIME = {0, 500000000};
 
   do {
     int FD_PWM_FAN;
-    char I2C_PWM_FAN_CHIP_ADDRESS = 0x2f;
-    char I2C_PWM_FAN_CHIP_REGISTER = 0x30;
-    char I2C_PWM_FAN_INIT_VALUE = 0x20;
-    string FILE_NAME = "/dev/i2c-10";
     char BUFFER[] = {I2C_PWM_FAN_CHIP_REGISTER, I2C_PWM_FAN_INIT_VALUE};
 
-    if ((FD_PWM_FAN = open(FILE_NAME.c_str(), O_RDWR)) < 0)
+    if ((FD_PWM_FAN = open(FAN_I2C_CHL, O_RDWR)) < 0)
       exit(1);
 
     if (ioctl(FD_PWM_FAN, I2C_SLAVE, I2C_PWM_FAN_CHIP_ADDRESS) < 0)
@@ -33,8 +36,12 @@ int main(void) {
     pread(FD_PWM_FAN, BUFFER, 1, 0);
     close(FD_PWM_FAN);
     const float sum = (int)BUFFER[0] / 255.0 * 100;
-    string tmp = " \"fan: %.2f\n\" " + to_string(sum);
-    system(string("printf").append(tmp).c_str());
+    const float fan_dba = FAN_MAX_DBA * (sum / 100);
+    const short fan_rpm = FAN_MAX_RPM * (sum / 100);
+    string fanSpeed = " \"Fan: %.2f%%, %drpm, %.2fdb(a) \n\" " +
+                      to_string(sum) + " " + to_string(fan_rpm) + " " +
+                      to_string(fan_dba);
+    system(string("printf").append(fanSpeed).c_str());
 
     nanosleep(&SLEEP_TIME, &SLEEP_TIME);
   } while (true);
